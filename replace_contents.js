d05157f301c7a452553574b5ceace0b24055d0d7
Stereowalker <35645830+Stereowalker@users.noreply.github.com>
2022-10-18 14:41:11 +0100

    Finished the Armor section

--- replace_contents.js
+++ replace_contents.js
@@ -4,13 +4,15 @@ function populatePre(contents, url) {
     xhr.onload = function () {
         contents.textContent = this.responseText;
         hljs.highlightElement(contents)
+        resizeCodeBlocks(contents.parentElement.parentElement, true);
     };
-    console.log(url+contents.id+".json")
-    xhr.open('GET', url+contents.id+".json");
+    console.log(url+contents.id)
+    xhr.open('GET', url+contents.id);
     xhr.send(null);
 }
 
 var codes = document.getElementsByClassName("dynamic_file");
 for (var i = 0; i < codes.length; i++) {
-    populatePre(codes.item(i), 'https://stereowalker.github.io/My-Home-Page/update_files/');
+    //populatePre(codes.item(i), 'https://stereowalker.github.io/My-Home-Page/files_to_load/');
+    populatePre(codes.item(i), 'http://myhomepage/files_to_load/');
 }
--- resize_code_block.js
+++ resize_code_block.js
@@ -6,19 +6,27 @@ window.onload = function () {
     }
 };
 //
+
 var divs = document.getElementsByClassName("parent_cb");
+
 for (var i = 0; i < divs.length; i++) {
-    var parent = divs.item(i);
+    resizeCodeBlocks(divs.item(i), false)
+}
+function resizeCodeBlocks(parent, resizedPadding) {
     var thisDiv = parent.children[0];
     for (var j = 0; j < thisDiv.children.length; j++) {
         var thatDiv = thisDiv.children[j];
         if (thatDiv.classList.contains('child_cb')) {
-            var padding = Math.ceil(2*parseFloat(getStyle(thatDiv).padding.slice(0, -2)))
-            var contentH = parseInt(getStyle(thisDiv).height.slice(0, -2)) + (padding*2)
+            var contentPadding = 0;
+            if (!resizedPadding)
+                contentPadding = Math.ceil(2*parseFloat(getStyle(thatDiv).padding.slice(0, -2)))
+            else
+                contentPadding = Math.ceil(parseFloat(getStyle(thatDiv).padding.slice(0, -2)))
+            var contentH = parseInt(getStyle(thisDiv).height.slice(0, -2)) + (contentPadding*2)
             var maxH = parseInt(getStyle(parent).maxHeight.slice(0, -2))
             if (contentH > maxH)
                 parent.style.height = maxH+"px"
-            thisDiv.style.height = "calc(100% - "+padding+"px)"
+            thisDiv.style.height = "calc(100% - "+contentPadding+"px)"
         }
     }
 }

a9ccb3cc7fa5727e56c6c87fa9774cb0088d3ca9
Stereowalker <stereowalker.mc@gmail.com>
2023-05-22 00:32:57 +0100

    Removed useless additions to the webpage

--- replace_contents.js
+++ replace_contents.js
@@ -19,6 +19,5 @@ var codes = document.getElementsByClassName("dynamic_file");
 for (var i = 0; i < codes.length; i++) {
     //Figure out cross domain transfers when you're using server software
     populatePre(codes.item(i), '../files_to_load/');
-    //populatePre(codes.item(i), 'https://stereowalker.com/files_to_load/');
     //populatePre(codes.item(i), 'http://myhomepage/files_to_load/');
 }

67f2f4099afe7eb5c9546ed78952dee322b05b16
Stereowalker <stereowalker.mc@gmail.com>
2023-05-21 15:19:09 +0100

    Attempt to manipulate files

--- replace_contents.js
+++ replace_contents.js
@@ -8,11 +8,17 @@ function populatePre(contents, url) {
     };
     console.log(url+contents.id)
     xhr.open('GET', url+contents.id);
+    xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+    xhr.setRequestHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+    xhr.setRequestHeader("Access-Control-Allow-Headers", "Content-Type");
+    //xhr.setRequestHeader("x-filename", photoId);
     xhr.send(null);
 }
 
 var codes = document.getElementsByClassName("dynamic_file");
 for (var i = 0; i < codes.length; i++) {
-    populatePre(codes.item(i), 'https://stereowalker.com/files_to_load/');
+    //Figure out cross domain transfers when you're using server software
+    populatePre(codes.item(i), '../survive/');
+    //populatePre(codes.item(i), 'https://stereowalker.com/files_to_load/');
     //populatePre(codes.item(i), 'http://myhomepage/files_to_load/');
 }
